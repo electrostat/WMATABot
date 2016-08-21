0bb8a740d2c44ddcbbd8213fa3036ac50f4a5e2a
electrostat <agbyaj@gmail.com>
2016-08-21 11:39:09 -0400

    Rail Arrivals
    - rail time responses working (metro center)
    - need to get all station codes
    - decider which station user wants

--- wmata.js
+++ wmata.js
@@ -1,8 +1,18 @@
 var Botkit = require('botkit');
+//var ajax = require('ajax');
+var http = require('http');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var controller = Botkit.slackbot();
 var bot = controller.spawn({
-	token: "api_token"
+	token: "slack_api_code"
 })
+
+//wmata stuff
+var wmata_api = 'wmata_api_code';
+//var params = {
+//            "api_key": "{662c841886354ff993c318078a7ccfdc}",
+//        };
+
 bot.startRTM(function(err,bot,payload) {
   if (err) {
     throw new Error('Could not connect to Slack');
@@ -51,4 +61,25 @@ controller.hears(['fire', ':fire:'], 'direct_message,direct_mention,mention', fu
             bot.reply(message, 'Of course :fire::fire::boom::fire::fire:');
         }
     });
+});
+
+controller.hears(['metro', 'wmata'], 'direct_message,direct_mention,mention,ambient', function(bot, message) {
+	bot.reply(message, "You rang?");
+});
+
+controller.hears(['test'], 'direct_message,direct_mention,mention,ambient', function(bot, message) {
+    
+    var xhttp = new XMLHttpRequest();
+        
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            bot.reply(message, "Success");
+            console.log("success: " + xhttp.responseText);
+        }
+    };
+        
+    xhttp.open('GET', "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/A01", true);
+    xhttp.setRequestHeader("api_key", wmata_api);
+    xhttp.send();
+	
 });
